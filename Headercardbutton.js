e477e3c267b93aac92fcaadb15e210b43cb7ea8a
riyasaxena09 <135497287+riyasaxena09@users.noreply.github.com>
2023-08-01 14:29:44 +0530

    add Headercardbutton.js

--- Headercardbutton.js
+++ Headercardbutton.js
@@ -1,13 +1,21 @@
 import CartIcon from "../Cart/CartIcon";
+import { useContext } from "react";
+import Cartcontext from "../../store/cart-context";
 import classes from './Headercardbutton.module.css';
 function Headercardbutton(props){
+  
+   const ctx=useContext(Cartcontext);
+   const num=ctx.items.reduce((concur,item)=>{
+     return concur+item.amount;
+   },0);
+    console.log(props)
     return(
-        <button className={classes.button}>
+        <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon/>
             </span>
             <span>Your cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{num}</span>
         </button>
     )
 }
